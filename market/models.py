42c831fdda00fb20cc1f79c986e21d7ede9e213e
Marat17 <muhutdinov@hotmail.com>
2019-01-13 17:26:29 +0300

    event and ticket models

--- market/models.py
+++ market/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=15)
+    slug = models.SlugField(unique=True)
+
+
+class Ticket(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    ticket_type = models.CharField(max_length=15)
+    price = models.IntegerField()
